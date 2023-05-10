e5e83a772366e1896ed804eea4f3add3c6f5fb5c
Furry <ender@thefluegges.com>
2023-05-09 22:30:27 -0400

    changes

--- src/routes/scaffolding/BaseRoute.ts
+++ src/routes/scaffolding/BaseRoute.ts
@@ -3,6 +3,8 @@ import { Database } from "../../utils/Database.js";
 import Logger from "../../utils/Logger.js";
 import { Api } from "./Api.js";
 import { Static } from "./Static.js";
+import fs from "fs";
+import path from "path";
 
 export function BaseRoute(db: Database) {
     const router = Router();
@@ -24,13 +26,20 @@ export function BaseRoute(db: Database) {
 
         next();
     });
-
-    router.get("/", (_, res) => {
+    
+    router.use("/api", Api(db));
+    router.use("/s", Static());
+    router.get("*", (req, res) => {
+        // Check if there's a matching file in the web directory. If so, serve it, else redirect to discord.
+        if (req.url != "/") {
+            let fname = req.url.split("/")[1];
+            if (fs.existsSync(`./static/web/${fname}.html`)) {
+                return res.sendFile(path.resolve(`./static/web/${fname}.html`));
+            }
+        }
         // Send to discord server
         res.redirect("https://discord.gg/tamVs2Ujrf");
     })
-    router.use("/api", Api(db));
-    router.use("/s", Static());
 
     return router;
 }
